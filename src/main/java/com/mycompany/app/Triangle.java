a3f53a66b4456b66846fa67f9271816ea0313544
LamTran461300 <50953306+LamTran461300@users.noreply.github.com>
2019-10-29 14:16:14 +0100

    Update App.java for better programming. Using buffered writer to print text instead of system and output, also avoid nested statement in class constructor by given the nested statement a temporary variable.

--- src/main/java/com/mycompany/app/Triangle.java
+++ src/main/java/com/mycompany/app/Triangle.java
@@ -1,21 +1,31 @@
 package com.mycompany.app;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.*;
+
 public class Triangle
 {
     private double small;
     private double medium;
     private double big;
 
-    public Triangle(double side1, double side2, double side3) throws IllegalArgumentException
+    public Triangle(double side1, double side2, double side3)
     {
         if((side1<=0||side2<=0)||side3<=0)
         {
             throw new IllegalArgumentException("One of the sides cannot be negative.");
         }
 
-        big = side1 >= side2 && side1 >= side3 ? side1 : side2 >= side1 && side2 >= side3 ? side2 : side3;
-        small = side1 <= side2 && side1 <= side3 ? side1 : side2 <= side1 && side2 <= side3 ? side2 : side3;
-        medium = side1 != big && side1 != small ? side1 : side2 != big && side2 != small ? side2 : side3;
+        double tempBig = side2 >= side1 && side2 >= side3 ? side2 : side3;
+        double tempSmall = side2 <= side1 && side2 <= side3 ? side2 : side3;
+        double tempMedium = side2 != big && side2 != small ? side2 : side3;
+
+        big = side1 >= side2 && side1 >= side3 ? side1 : tempBig;
+        small = side1 <= side2 && side1 <= side3 ? side1 : tempSmall;
+        medium = side1 != big && side1 != small ? side1 : tempMedium;
 
         if(side1==side2 && side3<side1)
         {
@@ -51,29 +61,38 @@ public class Triangle
         return (small==medium) && (medium==big);
     }
 
-    public void showType(){
-        if(isAcuteTriangle()){
-            if(isEquilateralTriangle())
-                System.out.println("This is an equilateral triangle");
-            else if(isIsoscelesTriangle())
-                System.out.println("This is an acute isosceles triangle");
+    public void showType() throws IOException {
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new
+                FileOutputStream(java.io.FileDescriptor.out), StandardCharsets.US_ASCII), 512);
+
+        try{
+            if(isAcuteTriangle()){
+                if(isEquilateralTriangle())
+                    out.write("This is an equilateral triangle\n");
+                else if(isIsoscelesTriangle())
+                    out.write("This is an acute isosceles triangle\n");
+                else
+                    out.write("This is an acute scalene triangle\n");
+            }
+            else if(isObtuseTriangle()){
+                if(isIsoscelesTriangle())
+                    out.write("This is an obtuse isosceles triangle\n");
+                else
+                    out.write("This is an obtuse scalene triangle\n");
+            }
+            else if(isRightTriangle()){
+                if(isIsoscelesTriangle())
+                    out.write("This is a right isosceles triangle\n");
+                else
+                    out.write("This is a right scalene triangle\n");
+            }
             else
-                System.out.println("This is an acute scalene triangle");
+                out.write("No triangle is formed with the given sides\n");
+
+            out.flush();
+        }finally {
+            out.close();
         }
-        else if(isObtuseTriangle()){
-            if(isIsoscelesTriangle())
-                System.out.println("This is an obtuse isosceles triangle");
-            else
-                System.out.println("This is an obtuse scalene triangle");
-        }
-        else if(isRightTriangle()){
-            if(isIsoscelesTriangle())
-                System.out.println("This is a right isosceles triangle");
-            else
-                System.out.println("This is a right scalene triangle");
-        }
-        else
-            System.out.println("No triangle is formed with the given sides");
     }
 
     public double getBig(){
